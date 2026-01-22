177ee9106d8d0128c86e3c0ae91f1d9c1ed3c83b
rajanpandey9742 <rajanpandey9742@gmail.com>
2026-01-21 21:08:02 -0600

    Added f2() method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,21 @@
 /**
  *
- * @author Your Name
+ * @author Rajan Pandey
  */
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
         System.out.println("Hello, World!");
     }
     
     private void f1() {
         // v1.0
     }
+public void f2() {
+    System.out.println("Feature 2");
+}
 
 }
